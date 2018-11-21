0381925b0b2c13fb2d9b88d8fe20e1805e526996
victorsmits <victor.smits@student.vinci.be>
2018-11-21 11:59:00 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Domotique.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Domotique.dir/main.c.o"
+  "Domotique.pdb"
+  "Domotique"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Domotique.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
